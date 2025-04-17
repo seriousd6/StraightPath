97621bde80107c6a3fb2a9a54ab9372f9da8d159
David Seis <85198545+David-Seis@users.noreply.github.com>
2025-04-17 11:19:40 -0400

    adding import and credentials

--- Tests/TestAppveyor.tests.ps1
+++ Tests/TestAppveyor.tests.ps1
@@ -1,12 +1,18 @@
+Import-Module dbatools
+import-Module dbatools.library
+
 Describe 'Hello World Tests' {
     It 'should return Hello World' {
         $result = 'Hello World'
         $result | Should -Be 'Hello World'
     }
 }
-Invoke-DbaQuery -SqlInstance $TestConfig.instance1 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
-Invoke-DbaQuery -SqlInstance $TestConfig.instance2 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
-Invoke-DbaQuery -SqlInstance $TestConfig.instance3 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
-Invoke-DbaQuery -SqlInstance $TestConfig.instance4 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
-Invoke-DbaQuery -SqlInstance $TestConfig.instance5 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
-Invoke-DbaQuery -SqlInstance $TestConfig.instance6 -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+
+$Credential = New-Object System.Management.Automation.PSCredential('sa', $(ConvertTo-SecureString 'Password12!' -AsPlainText -Force))
+
+Invoke-DbaQuery -SqlInstance $TestConfig.instance1 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+Invoke-DbaQuery -SqlInstance $TestConfig.instance2 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+Invoke-DbaQuery -SqlInstance $TestConfig.instance3 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+Invoke-DbaQuery -SqlInstance $TestConfig.instance4 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+Invoke-DbaQuery -SqlInstance $TestConfig.instance5 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
+Invoke-DbaQuery -SqlInstance $TestConfig.instance6 -SqlCredential $Credential -Query 'SELECT @@servername,@@version' #| Should -Not -BeNullOrEmpty
